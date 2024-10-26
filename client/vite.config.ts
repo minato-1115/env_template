8cac27f370ffab08f33fc8c45826be8fbfbe3f2d
minato-1115 <akasatana6789hama@gmail.com>
2024-10-26 12:36:10 +0900

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root:"client",
-  plugins: [react()],
+export default defineConfig(({mode})=>{
+  const isProduction = mode === "production"
+  return {
+    root:isProduction ? "client" : ".",
+    plugins: [react()]
+  }
 })
