1b84122df1c043158d282cae8bb1165b54f66cbf
minato-1115 <akasatana6789hama@gmail.com>
2024-10-26 13:13:26 +0900

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root:"client",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'client/index.html',  // 明示的にindex.htmlへのパスを指定
+    },
+  },
 })
