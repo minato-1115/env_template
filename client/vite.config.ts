be68d3d146eacc64bf175634818efc93d62d1153
minato-1115 <akasatana6789hama@gmail.com>
2024-10-26 13:53:31 +0900

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode})=>{
-  return{
+export default defineConfig({
   plugins: [react()],
-  base:mode ==="production"? "https://create-template-server-5510e22ac8f9.herokuapp.com/":"/"
-  }
+  base:process.env.VITE_APP_ENV ==="production"? "https://create-template-server-5510e22ac8f9.herokuapp.com/":"/"
 })
