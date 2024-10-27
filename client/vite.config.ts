25fa6da5a84d38e16dad591bb0fb27b27d522ac3
minato-1115 <akasatana6789hama@gmail.com>
2024-10-28 00:10:06 +0900

    commit vite.configの設定を変更。これでwebpackの中身が変更できたか？

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_APP_ENV ==="production"? "https://create-template-server-5510e22ac8f9.herokuapp.com/":"/"
+  base:process.env.VITE_APP_ENV ==="production"? "https://create-template-server-5510e22ac8f9.herokuapp.com/":"/",
+  build: {
+    sourcemap: process.env.VITE_APP_ENV !== "production" }
 })
